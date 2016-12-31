7b0888d569235d89a554df3aa328fdbe81054c82
srjahir32 <srjahir32@gmail.com>
2016-12-31 12:39:10 +0530

    update for web site

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# excel-to-json-in-Node.js
+The OpenShift `nodejs` cartridge documentation can be found at:
 
-## Tutorial 
-http://code.ciphertrick.com/2016/06/05/read-excel-files-convert-json-node-js/
-
-##Quick Setup
-
-1) `git clone https://github.com/rahil471/excel-to-json-in-Node.js.git` <br>
-2) `cd excel-to-json-in-Node.js` <br>
-3) `npm install` <br>
-4) `node app.js` <br>
-5) In your browser `http://localhost:3000` <br>
-6) Upload excel file and see result <br>
+http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
--- app.js
+++ app.js
@@ -1,35 +1,33 @@
- var express = require('express'); 
-    var app = express(); 
-    var bodyParser = require('body-parser');
-    var multer = require('multer');
-    var excel=require(__dirname +'/api/excel.js');
-    var vcf=require(__dirname +'/api/VcftoExcel.js');
-    var vcftovcf=require(__dirname +'/api/vcftovcf.js');
-    var vcftocsv=require(__dirname +'/api/VcftoCsv.js');
-    var email=require(__dirname +'/api/email.js');
-    var router = express.Router();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var excel = require(__dirname + '/api/excel.js');
+var vcf = require(__dirname + '/api/VcftoExcel.js');
+var vcftovcf = require(__dirname + '/api/vcftovcf.js');
+var vcftocsv = require(__dirname + '/api/VcftoCsv.js');
+var email = require(__dirname + '/api/email.js');
+var router = express.Router();
+var http = require('http');
 
-    app.use(bodyParser.json());  
- 
-router.get('/test',function(req,res)
-{
-    res.sendFile(__dirname + "/index.html");
-});
+app.use(bodyParser.json());
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 
-router.get('/',function(req,res)
-{
+router.get('/', function (req, res) {
     res.sendFile(__dirname + "/view/index.html");
 });
 
-app.post('/exceltovcf',excel.exceltovcf);
-app.post('/getExcelData',excel.getExcelData);
+app.post('/exceltovcf', excel.exceltovcf);
+app.post('/getExcelData', excel.getExcelData);
 
-app.post('/vcftoEcel',vcf.vcftoexcel);
+app.post('/vcftoEcel', vcf.vcftoexcel);
 
-app.post('/vcftovcf',vcftovcf.vcftovcf);
-app.post('/vcftocsv',vcftocsv.vcftoCsv);
-app.post('/sendmail',email.sendmail);
+app.post('/vcftovcf', vcftovcf.vcftovcf);
+app.post('/vcftocsv', vcftocsv.vcftoCsv);
+app.post('/sendmail', email.sendmail);
 
 
 app.use('/', router);
@@ -39,13 +37,13 @@ app.use("/npm", express.static(__dirname + '/node_modules'));
 app.use("/images", express.static(__dirname + '/view/images'));
 app.use("/js", express.static(__dirname + '/view/assets/js'));
 app.use("/css", express.static(__dirname + '/view/assets/css'));
-app.use("/fonts",express.static(__dirname+'/view/assets/fonts'));
+app.use("/fonts", express.static(__dirname + '/view/assets/fonts'));
 app.use("/assets", express.static(__dirname + '/view/assets'));
 app.use("/view", express.static(__dirname + '/view'));
 app.use("/uploads", express.static(__dirname + '/uploads'));
-app.use("/tpl",express.static(__dirname+'/view/assets/tpl'));
+app.use("/tpl", express.static(__dirname + '/view/assets/tpl'));
 
 
-    app.listen('3000', function(){
-        console.log('running on 3000...');
-    });
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
